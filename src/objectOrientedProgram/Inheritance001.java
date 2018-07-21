e7e2ad75934679f99e5b268d17a248ce927f993c
Mosarrof Hossain <phmosharaf@gmail.com>
2018-07-20 22:05:49 -0400

    Done

--- src/objectOrientedProgram/Inheritance001.java
+++ src/objectOrientedProgram/Inheritance001.java
@@ -5,14 +5,18 @@ public class Inheritance001 {
 	public static void main(String[] args) {
 		
  Student s = new Student();
+ Result r = new Result();
  s.name = " Monium";
  s.roll = 101;
  System.out.println(" Name : " +s.name);
  System.out.println(" Name : " +s.roll);
 		 
  System.out.println(" Class Name of S : " + s.getClass());
- System.out.println(" Class Name of S : " + s.toString());
+ System.out.println(" Memory location of S object : " + s.toString());
  System.out.println(" Class Name of S : " + s.equals("It's me"));
+ System.out.println(" Roll : " + r.roll);
+ System.out.println(" Class Name of r : " + r.getClass());
+ System.out.println(" Memory location of r object : " + r.toString());
 	}
 
 }
