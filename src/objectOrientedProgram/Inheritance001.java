0c38bfe53edc0a367d3844e82ca5ed9d4fd4d952
Mosarrof Hossain <phmosharaf@gmail.com>
2018-07-20 22:34:18 -0400

    Object get the result

--- src/objectOrientedProgram/Inheritance001.java
+++ src/objectOrientedProgram/Inheritance001.java
@@ -3,28 +3,42 @@ package objectOrientedProgram;
 public class Inheritance001 {
 
 	public static void main(String[] args) {
+
+		Student s = new Student();
+		Result r = new Result();
+
+		s.name = " Monium";
+		s.roll = 101;
+
+		System.out.println(" Name : " +s.name);
+		System.out.println(" Name : " +s.roll);
+
+		r.roll = 102;
+		r.name = "Mahdi";
+		r.mark = 99;
+
+		System.out.println();
 		
- Student s = new Student();
- Result r = new Result();
- s.name = " Monium";
- s.roll = 101;
- System.out.println(" Name : " +s.name);
- System.out.println(" Name : " +s.roll);
-		 
- System.out.println(" Class Name of S : " + s.getClass());
- System.out.println(" Memory location of S object : " + s.toString());
- System.out.println(" Class Name of S : " + s.equals("It's me"));
- System.out.println(" Roll : " + r.roll);
- System.out.println(" Class Name of r : " + r.getClass());
- System.out.println(" Memory location of r object : " + r.toString());
- 
- System.out.println("Is S object inheritance of Student class ?  " +( s instanceof Student));
- System.out.println("Is S object inheritance of Student class ?  " +( s instanceof Result));
- System.out.println("Is S object inheritance of Student class ?  " +( r instanceof Student));
- System.out.println("Is S object inheritance of Student class ?  " +( r instanceof Result));
- 
- System.out.println("Is S object inheritance of Student class ?  " +( s instanceof Object));
- System.out.println("Is S object inheritance of Student class ?  " +( r instanceof  Object));
+		System.out.println(" Class Name of S : " + s.getClass());
+		System.out.println(" Memory location of S object : " + s.toString());
+		System.out.println(" Class Name of S : " + s.equals("It's me"));
+
+		System.out.println();
+		
+		System.out.println(" Roll : " + r.roll);
+		System.out.println(" Roll : " + r.name);
+		System.out.println(" Roll : " + r.mark);
+
+		System.out.println(" Class Name of r : " + r.getClass());
+		System.out.println(" Memory location of r object : " + r.toString());
+
+		System.out.println("Is S object inheritance of Student class ?  " +( s instanceof Student));
+		System.out.println("Is S object inheritance of Student class ?  " +( s instanceof Result));
+		System.out.println("Is S object inheritance of Student class ?  " +( r instanceof Student));
+		System.out.println("Is S object inheritance of Student class ?  " +( r instanceof Result));
+
+		System.out.println("Is S object inheritance of Student class ?  " +( s instanceof Object));
+		System.out.println("Is r object inheritance of Student class ?  " +( r instanceof Object));
 	}
 
 }
